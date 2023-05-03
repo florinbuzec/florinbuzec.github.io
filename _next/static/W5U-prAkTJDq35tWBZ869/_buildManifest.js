3eafe5fa8de639c0ce62e147d52c76ce71584e42
DjBpF <bpfcomp2005@gmail.com>
2023-05-03 20:04:15 +0300

    updated pe verde

--- _next/static/W5U-prAkTJDq35tWBZ869/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0c831bb7df4fad53.css","static/chunks/pages/index-48992a1eb70d05bd.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/hltJXCTojwJqhYMS_9VWk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bae8300bbe7cd850.css","static/chunks/pages/index-9a0b99eabbbc4915.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
